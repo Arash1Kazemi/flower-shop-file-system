5b42dcfda3a98b5365a6dbb08fdbdb170d857f05
parsa <parsaabu@gmail.com>
2025-10-03 20:44:47 +0330

    customer menu

---
+++ customer_menu.c
@@ -0,0 +1,51 @@
+#include "shop.h"
+#include <time.h>
+
+// Function to show the post-login customer menu
+void customer_menu(Customer *c) {
+  int choice;
+
+  while (1) {
+    printf("\n--- Customer Menu ---\n");
+    printf("Welcome, %s!\n", c->name);
+    printf("1 - List Flowers\n");
+    printf("2 - List Pots\n");
+    printf("3 - Buy Flower\n");
+    printf("4 - Buy Pot\n");
+    printf("5 - View Receipts / Purchase History\n");
+    printf("6 - Logout\n");
+
+    if (!get_valid_int("Your choice: ", &choice))
+      continue;
+
+    switch (choice) {
+    case 1:
+      list_flowers();
+      break;
+
+    case 2:
+      list_pots();
+      break;
+
+    case 3:
+      buy_flower(c);
+      break;
+
+    case 4:
+      buy_pot(c);
+      break;
+
+    case 5:
+      view_receipts(c);
+      break;
+
+    case 6:
+      printf("logging out...\n");
+      return;
+
+    default:
+      printf("Invalid choice.\n");
+    }
+  }
+}
+
